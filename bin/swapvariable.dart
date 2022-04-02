4d99d3f2f6f73cb72c8f0e100040a40f3092284c
mansiitrapasiya <trapasiyamansi304@gmail.com>
2022-04-02 11:42:07 +0530

    change code

--- bin/swapvariable.dart
+++ bin/swapvariable.dart
@@ -2,11 +2,14 @@ import 'dart:io';
 //import 'dart:math';
 
 void main() {
-  String num1, num2;
+  int num1, num2;
   stdout.write("enter the first num:");
-  num1 = (stdin.readLineSync()!);
+  num1 = int.parse(stdin.readLineSync()!);
   stdout.write("enter the second num:");
-  num2 = (stdin.readLineSync()!);
-  num1 = num2;
-  print("after swapping,num1:$num1,num2:$num2");
+  num2 = int.parse(stdin.readLineSync()!);
+  num1 = num1 + num2;
+  num2 = num1 - num2;
+  num1 = num1 - num2;
+
+  print("after swapping \n num1:$num1 \n num2:$num2");
 }
